0eed66874ec85151b246ae7a3ca3d63778bf23ee
MKashanButt <kashanbutt566@gmail.com>
2026-03-18 02:19:14 +0500

    since stock id could be null add it to gracefully handle the null use case

--- resources/views/admin/payment/edit.blade.php
+++ resources/views/admin/payment/edit.blade.php
@@ -20,7 +20,7 @@
                         <div class="w-4/5 flex gap-1">
                             <x-pill>SKI-</x-pill>
                             <x-text-input type="text" id="stock_id" name="stock_id" class="flex-1"
-                                value="{{ old('stock_id', $payment->stock->sid) }}"
+                                value="{{ old('stock_id', $payment->stock->sid ?? '') }}"
                                 placeholder="Enter Stock Id number with no preceding zero's" />
                         </div>
                         <x-input-error :messages="$errors->get('stock_id')" class="mt-2" />
